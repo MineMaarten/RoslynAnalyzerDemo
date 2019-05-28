c74da4287ecb779ba0f47c4613c2567ee5e4678d
Maarten Kok <maarten.kok@avivasolutions.nl>
2019-05-28 13:59:27 +0200

    Added 'FixingProject' as example code to fix.

---
+++ FixingProject/Foo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FixingProject
+{
+    public class Foo<Bar> where Bar : class
+    {
+        public Bar CurrentBar { get; }
+
+        public Foo(Bar bar) 
+            => CurrentBar = bar ?? throw new ArgumentNullException(nameof(bar));
+
+        private class SomeNestedClass
+        {
+            public int Bar { get; set; }
+        }
+    }
+}
