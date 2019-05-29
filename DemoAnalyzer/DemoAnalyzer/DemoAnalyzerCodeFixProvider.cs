8b5745a61d9b42de8d7374df51682f28cd3d3bdc
Maarten Kok <maarten.kok@avivasolutions.nl>
2019-05-29 10:34:31 +0200

    Locate the type declaration syntax identified by the diagnostic.

--- DemoAnalyzer/DemoAnalyzer/DemoAnalyzerCodeFixProvider.cs
+++ DemoAnalyzer/DemoAnalyzer/DemoAnalyzerCodeFixProvider.cs
@@ -30,12 +30,11 @@ namespace DemoAnalyzer
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
 
-            // Find the type declaration identified by the diagnostic.
-            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
+            // Find the type parameter declaration identified by the diagnostic.
+            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeParameterSyntax>().First();
 
             // Register a code action that will invoke the fix.
             context.RegisterCodeFix(
